619ef977eab650624e0de21b8ac9f0cb9b181257
SerPBushmakin <sergey1989ok@mail.ru>
2025-10-19 21:05:01 +0400

    Add exceptions

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,7 +1,7 @@
 // Класс для описания велосипеда
 final class Bicycle extends Transport {
     public Bicycle(String name) {
-        super(name, 0, FuelType.ELECTRIC); // Велосипед не имеет мощности, используем электрический
+        super(name, 0, FuelType.ELECTRIC); // для Велосипеда используем только электричество
     }
 
     @Override
